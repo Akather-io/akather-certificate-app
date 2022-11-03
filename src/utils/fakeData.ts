72b61eb93397ceebfc0354c4f1d31b7e97ff122b
leopham <hongthaipro@gmail.com>
2022-11-03 16:24:32 +0700

    course page

---
+++ src/utils/fakeData.ts
@@ -0,0 +1,34 @@
+export const coursesData = [
+  {
+    id: 1,
+    name: "Surgical Skill 1",
+  },
+  {
+    id: 2,
+    name: "Surgical Skill 2",
+  },
+  {
+    id: 3,
+    name: "Surgical Skill 3",
+  },
+  {
+    id: 4,
+    name: "Surgical Skill 4",
+  },
+  {
+    id: 5,
+    name: "Surgical Skill 5",
+  },
+  {
+    id: 6,
+    name: "Surgical Skill 6",
+  },
+  {
+    id: 7,
+    name: "Surgical Skill 7",
+  },
+  {
+    id: 8,
+    name: "Surgical Skill 8",
+  },
+] as const;
