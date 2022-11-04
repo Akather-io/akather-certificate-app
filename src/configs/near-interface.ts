797dec9a6dcdc2e256d5d2796ef89f338301bf72
leopham <hongthaipro@gmail.com>
2022-11-04 13:00:32 +0700

    demo 1

--- src/configs/near-interface.ts
+++ src/configs/near-interface.ts
@@ -1,7 +1,12 @@
 /* Talking with a contract often involves transforming data, we recommend you to encapsulate that logic into a class */
 
-import { ICertificate } from "../app/my-certificate/certificate.type";
+import {
+  ICertificate,
+  ITokenMetadata,
+} from "../app/my-certificate/certificate.type";
 import { Wallet } from "./near-wallet";
+
+import { utils } from "near-api-js";
 
 export class NftCertificateNEAR {
   contractId: string;
@@ -25,6 +30,29 @@ export class NftCertificateNEAR {
       args: {
         account_id: this.wallet.accountId,
       },
+    });
+  }
+
+  nft_mint(user: string, tokenId?: string) {
+    const time = Math.floor(new Date().getTime() / 1000);
+    const newMetadata: ITokenMetadata = {
+      title: "Surgical Skill Certificate",
+      description:
+        "has successfully completed the Surgical Skill program by Akather",
+      media:
+        "https://bafybeifcqlnqvgefmrnxpe7lf4p4tv2y3dl25l67ka3onsakqmd2ch4ksq.ipfs.dweb.link",
+      issued_at: time,
+      extra: JSON.stringify({ issued_by: "Akather" }),
+    };
+    return this.wallet.callMethod({
+      contractId: this.contractId,
+      method: "nft_mint",
+      args: {
+        token_id: tokenId || `surgical-kill-${time.toFixed(0)}`,
+        metadata: newMetadata,
+        receiver_id: user,
+      },
+      deposit: utils.format.parseNearAmount("0.01")!,
     });
   }
 }
